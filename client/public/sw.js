7267e932f4be976a811bf139123ecf1f445347e6
Hamid Tlailia <tlhamid18@gmail.com>
2026-09-02 09:50:44 +0300

    Add files via upload

---
+++ client/public/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "mizan-hadith-v2";
+const CORE_ASSETS = ["/", "/manifest.webmanifest", "/icons/mizan-192.png", "/icons/mizan-512.png", "/icons/mizan-maskable-512.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(match => match || caches.match("/"))));
+});
